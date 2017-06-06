53d4cd72a71a470a60d5216b7b0296cf11a27b4f
Connor Larsen and Daniela Trulls <connor@>
2017-06-06 11:54:11 -0700

    Add structure in index.html, add architecture for /sass

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Show sideNav
+$(document).ready(function(){
+
+  // Initialize collapse button
+    $(".button-collapse").sideNav();
+
+ // $(".button-collapse").sideNav();
+$('.button-collapse').sideNav('show');
+// Hide sideNav
+$('.button-collapse').sideNav('hide');
+// Destroy sideNav
+$('.button-collapse').sideNav('destroy');
+
+});
